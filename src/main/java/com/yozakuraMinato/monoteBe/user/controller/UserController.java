e4b2842e1f80381ac7c948a666b4ebb9e9b488d6
yozakura-minato <huynhhuulocht@gmail.com>
2026-06-27 15:56:19 +0700

    small format refactors

--- src/main/java/com/yozakuraMinato/monoteBe/user/controller/UserController.java
+++ src/main/java/com/yozakuraMinato/monoteBe/user/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.yozakuraMinato.monoteBe.user.controller;
 
 import com.yozakuraMinato.monoteBe.common.wrapper.ApplicationResponse;
+import com.yozakuraMinato.monoteBe.security.service.SecurityContextApiService;
 import com.yozakuraMinato.monoteBe.user.controller.dto.SignInRequest;
-import com.yozakuraMinato.monoteBe.user.controller.dto.SignUpRequest;
 import com.yozakuraMinato.monoteBe.user.controller.dto.SignInResponse;
+import com.yozakuraMinato.monoteBe.user.controller.dto.SignUpRequest;
 import com.yozakuraMinato.monoteBe.user.controller.dto.SignUpResponse;
 import com.yozakuraMinato.monoteBe.user.service.UserApplicationService;
+import com.yozakuraMinato.monoteBe.user.shared.UserMessage;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,16 +16,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.UUID;
 
 @RestController
 public class UserController {
 
     @Autowired
-    private UserApplicationService userService;
+    private UserApplicationService userApplicationService;
+
+    @Autowired
+    private SecurityContextApiService securityContextApiService;
 
     @PostMapping("/sign-up")
     public ResponseEntity<ApplicationResponse<SignUpResponse>> signUp(@RequestBody @Valid SignUpRequest signUpRequest) {
-        SignUpResponse signUpResponse = userService.signUp(signUpRequest);
+        SignUpResponse signUpResponse = userApplicationService.signUp(signUpRequest);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(ApplicationResponse.data(signUpResponse));
@@ -31,17 +39,20 @@ public class UserController {
 
     @PostMapping("/sign-in")
     public ResponseEntity<ApplicationResponse<SignInResponse>> signIn(@RequestBody @Valid SignInRequest signInRequest) {
-        SignInResponse signInResponse = userService.signIn(signInRequest);
+        SignInResponse signInResponse = userApplicationService.signIn(signInRequest);
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(ApplicationResponse.data(signInResponse));
     }
 
     @GetMapping("/test")
-    public ResponseEntity<ApplicationResponse<String>> test() {
+    public ResponseEntity<ApplicationResponse<UUID>> test() {
+        UUID userId = securityContextApiService
+                .getUserId()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, UserMessage.Id.notFound));
         return ResponseEntity
                 .status(HttpStatus.OK)
-                .body(ApplicationResponse.data("Hello world!"));
+                .body(ApplicationResponse.data(userId));
     }
 
 }
