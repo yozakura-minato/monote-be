611109a594f0a2066449997f0c301433b9eeeae6
yozakura-minato <huynhhuulocht@gmail.com>
2026-06-28 22:12:44 +0700

    make inner class be static

--- src/main/java/com/yozakuraMinato/monoteBe/user/shared/UserConstant.java
+++ src/main/java/com/yozakuraMinato/monoteBe/user/shared/UserConstant.java
@@ -2,12 +2,12 @@ package com.yozakuraMinato.monoteBe.user.shared;
 
 public class UserConstant {
 
-    public class Password {
+    public static class Password {
         public static final int minimalSize = 8;
         public static final int maximalSize = 64;
     }
 
-    public class DisplayName {
+    public static class DisplayName {
         public static final int maximalSize = 32;
     }
 
--- src/main/java/com/yozakuraMinato/monoteBe/user/shared/UserMessage.java
+++ src/main/java/com/yozakuraMinato/monoteBe/user/shared/UserMessage.java
@@ -8,24 +8,24 @@ public class UserMessage {
     private static final String password = user + "password.";
     private static final String displayName = user + "displayName.";
 
-    public class Id {
+    public static class Id {
         public static final String notFound = id + "notFound";
     }
 
-    public class Email {
+    public static class Email {
         public static final String isNull = email+ "isNull";
         public static final String isInvalid = email + "isInvalid";
         public static final String alreadyExists = email + "alreadyExists";
         public static final String notFound = email + "notFound";
     }
 
-    public class Password {
+    public static class Password {
         public static final String isNull = password + "isNull";
         public static final String hasInvalidSize = password + "hasInvalidSize";
         public static final String isWeak = password + "isWeak";
     }
 
-    public class DisplayName {
+    public static class DisplayName {
         public static final String isNull = displayName + "isNull";
         public static final String hasInvalidSize = displayName + "hasInvalidSize";
     }
